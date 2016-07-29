5ca51a6ab8fe58aba2b8170da18fffe0d63d83ff
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-07-29 10:38:40 +0900

    pluginKeyの直書き指定不要になった - blockSetting共通化対応

--- Model/QuestionnaireSetting.php
+++ Model/QuestionnaireSetting.php
@@ -34,7 +34,6 @@ class QuestionnaireSetting extends BlockBaseModel {
 		'Blocks.BlockRolePermission',
 		'Blocks.BlockSetting' => array(
 			BlockSettingBehavior::FIELD_USE_WORKFLOW,
-			BlockSettingBehavior::SETTING_PLUGIN_KEY => 'questionnaires',
 		),
 	);
 
